93211618b50e20dcc2a0aec9ae5f7fca7b67fe0c
luborliu <luborliu@gmail.com>
2015-06-17 17:54:49 -0300

    remv redundant code

--- src/boliu/Main.java
+++ src/boliu/Main.java
@@ -27,7 +27,11 @@ public class Main {
 			tcd.onOpenDocument(args[0]);
 			tcd.onClusterGenerate(args[1], p.epsParam, p.minLnsParam);									
 		} else {
-			System.out.println("Please give me 2 or 4 input parameters!");
+			System.out.println("Please give me 2 or 4 input parameters! \n "
+					+ "If you have no idea how to decide eps and minLns, just feed in 2 parameters (inputFilePath, outputFilePath):\n"
+					+ "--e.g. java boliu.Main deer_1995.tra testOut.txt \n"
+					+ "If you know the two parameters, just feed in all the 4 parameters (inputFilePath, outputFilePath, eps, minLns)"
+					+ "--e.g. java boliu.Main deer_1995.tra testOut.txt 29 8 \n");
 		}
 		
 
